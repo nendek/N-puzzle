d036f12316523fcc7ae41d7d9528b44460288065
Adrien ROBION <arobion@e2r4p3.42.fr>
2019-04-16 16:18:42 +0200

    Improve data struct + prepare for A*

--- npuzzle_graph.py
+++ npuzzle_graph.py
@@ -1,9 +1,13 @@
+from math import floor
+
 class NpuzzleGraph():
     def __init__(self, len_puzzle, puzzle, objectif):
         self.len = len_puzzle
         self.puzzle = puzzle
         self.objectif = objectif
-        self.simulation = []
+#        self.simulation = []
+        self.closed = set()
+        self.open = []
 
     def __str__(self):
         ret = ""
@@ -13,12 +17,13 @@ class NpuzzleGraph():
         for i in range(self.len):
             ret += "\t{}\n".format(self.puzzle[i * self.len:(i + 1) * self.len])
         
-        ret += "current =\n"
-        if len(self.simulation) > 0:
-            for i in range(self.len):
-                ret += "\t{}\n".format(self.simulation[i * self.len:(i + 1) * self.len])
-        else:
-            ret += "\tNone\n"
+#        ret += "current =\n"
+#        if len(self.simulation) > 0:
+#            for i in range(self.len):
+#                ret += "\t{}\n".format(self.simulation[i * self.len:(i + 1) * self.len])
+#        else:
+#            ret += "\tNone\n"
+
         ret += "objectif =\n"
         for i in range(self.len):
             ret += "\t{}\n".format(self.objectif[i * self.len:(i + 1) * self.len])
@@ -26,24 +31,24 @@ class NpuzzleGraph():
         return ret
 
     def heuristique_placement(self):
-    total = 0
-    for index in range(0, len(self.simulation)):
-        if self.simulation[index] != self.objectif[index]:
-            total += 1
-    return total
-
-    def heuristique_nb_coups(self):
         total = 0
         for index in range(0, len(self.simulation)):
-            total += abs(floor(self.objectif.index(self.simulation[index]) % self.len) - floor(index % self.len)) + abs(floor(self.objectif.index(self.simulation[index]) / self.len) - floor(index / self.len))
+            if self.simulation[index] != self.objectif[index]:
+                total += 1
+        return total
+
+    def heuristique_nb_coups(self, puzzle):
+        total = 0
+        for index in range(0, len(puzzle)):
+            total += abs(floor(self.objectif.index(puzzle[index]) % self.len) - floor(index % self.len)) + abs(floor(self.objectif.index(puzzle[index]) / self.len) - floor(index / self.len))
         return total
     
-    def swap(pos1, pos2, lst):
+    def swap(self, pos1, pos2, lst):
         tmp = lst[pos1]
         lst[pos1] = lst[pos2]
         lst[pos2] = tmp
     
-    def move_puzzle(self, direction):
+    def move_puzzle(self, direction, simulation):
         """
         1 = left
         2 = bot
@@ -51,42 +56,42 @@ class NpuzzleGraph():
         4 = up
         """
         if direction == 1:
-            swap(self.simulation.index(0), self.simulation.index(0) - 1, self.simulation)
+            self.swap(simulation.index(0), simulation.index(0) - 1, simulation)
         if direction == 2:
-            swap(self.simulation.index(0), self.simulation.index(0) + self.len, self.simulation)
+            self.swap(simulation.index(0), simulation.index(0) + self.len, simulation)
         if direction == 3:
-            swap(self.simulation.index(0), self.simulation.index(0) + 1, self.simulation)
+            self.swap(simulation.index(0), simulation.index(0) + 1, simulation)
         if direction == 4:
-            swap(self.simulation.index(0), self.simulation.index(0) - self.len, self.simulation)
+            self.swap(simulation.index(0), simulation.index(0) - self.len, simulation)
 
-    def get_better_next_state(self):
+    def handle_next_state(self, state):
         """
         Pour l'instant cette fonction prend juste le meilleur h pour le prochain coup
         """
-        score_left = -1
-        score_right = -1
-        score_up = -1
-        score_bot = -1
         
         index_0 = self.puzzle.index(0)
         # try left move
         if index_0 % self.len != 0:
-            self.simulation = self.puzzle.copy()
-            move_puzzle(self, 1)
+            simulation = state.puzzle.copy()
+            self.move_puzzle(1, simulation)
+            print(simulation)
         
         # try bot move
         if floor(index_0 / self.len) != self.len:
-            self.simulation = self.puzzle.copy()
-            move_puzzle(self, 2)
+            simulation = self.puzzle.copy()
+            self.move_puzzle(2, simulation)
+            print(simulation)
             
         # try right move
         if index_0 % self.len != self.len:
-            self.simulation = self.puzzle.copy()
-            move_puzzle(self, 3)
+            simulation = self.puzzle.copy()
+            self.move_puzzle(3, simulation)
+            print(simulation)
             
         # try up move
         if floor(index_0 / self.len) != 0:
-            self.simulation = self.puzzle.copy()
-            move_puzzle(self, 4)
+            simulation = self.puzzle.copy()
+            self.move_puzzle(4, simulation)
+            print(simulation)
     
 
---
+++ npuzzle_state.py
@@ -0,0 +1,19 @@
+class NpuzzleState():
+    def __init__(self, puzzle, len_puzzle, g, h):
+        self.puzzle = puzzle
+        self.f = h + g
+        self.g = g
+        self.h = h
+        self.tuple = tuple(puzzle)
+        self.len = len_puzzle
+    
+    def __str__(self):
+        ret = ""
+        ret += "f(x) = {}\n".format(self.f)
+        ret += "g(x) = {}\n".format(self.g)
+        ret += "h(x) = {}\n".format(self.h)
+        ret += "puzzle =\n"
+        for i in range(self.len):
+            ret += "\t{}\n".format(self.puzzle[i * self.len:(i + 1) * self.len])
+        
+        return ret
